0d35090bfde1727c0bd69c4d4a6f823136ecd30a
nipaaa <mohiuddinkhan1409@gmail.com>
2024-01-23 22:50:45 +0600

    multiple styles, box model

--- App.js
+++ App.js
@@ -6,7 +6,13 @@ export default function App() {
 
   // return <CoreComponents />
   return <View style={styles.container}>
-    <Text>StyleSheet API</Text>
+    <View style={[styles.box, styles.lightblueBg]}>
+      <Text>Lightblue box</Text>
+    </View>
+    <View style={[styles.box, styles.lightGreenBg]}>
+      <Text>Lightgreen box</Text>
+    </View>
+
   </View>
 }
 
@@ -15,6 +21,23 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "plum",
     padding: 60
+  },
+  box: {
+    width: 100,
+    height: 100,
+    // padding: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    marginVertical: 10,
+    borderWidth: 2,
+    borderColor: "purple",
+    borderRadius: 5,
+  },
+  lightblueBg: {
+    backgroundColor: "lightblue",
+  },
+  lightGreenBg: {
+    backgroundColor: "lightgreen",
   }
 })
 
